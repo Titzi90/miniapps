ac6ad77202e09134db32d124187f5a44b63c669e
Christopher Bross <christopher.bross@fau.de>
2014-10-04 01:01:41 +0200

    change some output things

--- HPCG/testing/test.cpp
+++ HPCG/testing/test.cpp
@@ -60,14 +60,14 @@ int main(int argc, char * argv[]) {
 #ifndef HPCG_NOHPX
 if(0 == hpx::get_locality_id())
 {
-  hpx::cout<<"Using HPX"<<hpx::endl;
+  hpx::cerr<<"Using HPX"<<hpx::endl;
 }
 #endif
 #ifndef HPCG_NOOPENMP
-  std::cout<<"Using OMP"<<std::endl;
+  std::cerr<<"Using OMP"<<std::endl;
 #endif
 #ifndef HPCG_NOMPI
-  std::cout<<"Using MPI"<<std::endl;
+  std::cerr<<"Using MPI"<<std::endl;
 #endif
 
   HPCG_Params params;
@@ -151,22 +151,22 @@ if(0 == hpx::get_locality_id())
   double t7 = mytimer(); OptimizeProblem(A, data, b, x, xexact); t7 = mytimer() - t7;
   times[7] = t7;
 
-#ifndef HPCG_NOHPX
-  hpx::cout << "init optmization taks " << times[7] << hpx::endl;
-#endif
 #ifdef HPCG_DEBUG
   if (rank==0) HPCG_fout << "Total problem setup time in main (sec) = " << mytimer() - t1 << endl;
+  if (rank==0) std::cerr << "Total problem setup time in main (sec) = " << mytimer() - t1 << endl;
   if (rank==0) HPCG_fout << "Optimization setuptime (sec) = " << times[7] << endl;
+  if (rank==0) std::cerr << "Optimization setuptime (sec) = " << times[7] << endl;
 #endif
 
 
 /******************************************************************************/
 #ifndef HPCG_NOHPX
-  hpx::cout << "we have " << size << " localitys with " 
+  hpx::cerr << "we have " << size << " localitys with " 
             << params.numThreads << " hpx threads" <<hpx::endl;
 
 
-  hpx::cout << "startin init test..." << hpx::endl;
+#ifdef HPCG_DEBUG
+  hpx::cerr << "startin init test..." << hpx::endl;
 // compare data (ohne MG data)
   HPX_ASSERT(b_ref.localLength == b.localLength);
   HPX_ASSERT(x_ref.localLength == x.localLength);
@@ -194,23 +194,25 @@ if(0 == hpx::get_locality_id())
         HPX_ASSERT(A_ref.mtxIndL[i][j] == A.mtxIndL[i][j]);
     }
   }
-  hpx::cout << "init test ok!" <<hpx::endl;
+  hpx::cerr << "init test ok!" <<hpx::endl;
+#endif
 
-//TODO optidata wirklich richti
   std::vector<SubVector>  & subBs =
         *static_cast<std::vector<SubVector>* >(x.optimizationData);
 hpx::cerr << "We have " << subBs.size() << " sub domains" << hpx::endl;
+#ifdef HPCG_DEBUG
   for(size_t i=0; i<subBs.size(); ++i){
       VectorValues_future values_f = subBs[i].values_f;
       std::vector<double*> values = values_f.get();
       HPX_ASSERT(values.size() == subBs[i].localLength);
   }
+#endif
 
 #endif
 
 /******************************************************************************/
 // SPMV TEST
-  std::cout << "starting SPMV test" <<std::endl;
+  std::cerr << "starting SPMV test" <<std::endl;
   //reverence
   double ref_time = mytimer();
   ComputeSPMV_ref(A_ref, b_ref, x_ref);
@@ -220,21 +222,26 @@ hpx::cerr << "We have " << subBs.size() << " sub domains" << hpx::endl;
   //define and reset counter
   hpx::performance_counters::performance_counter idle_rate (
           "/threads{localiti#0/total}/idle-rate");
-  hpx::cout << "idle rate befor reset: " << idle_rate.get_value_sync<int>() << hpx::endl;
+  hpx::performance_counters::performance_counter thread_counter (
+          "/threads{localiti#0/total}/count/cumulative");
   idle_rate.reset_sync();
-  hpx::cout << "idle rate after reset: " << idle_rate.get_value_sync<int>() << hpx::endl;
+  thread_counter.reset_sync();
   double opt_time = mytimer();
   ComputeSPMV    (A   , b     , x    );
   hpx::wait_all(when_vec(x));
   opt_time = mytimer() - opt_time;
-  hpx::cout << "idle rate: " << idle_rate.get_value_sync<int>() << hpx::endl;
+  hpx::cout << "idle rate: " << 0.01 * idle_rate.get_value_sync<int>() << "%"<< hpx::endl;
+  hpx::cout << thread_counter.get_value_sync<int>() << " number of hpx threads"<< hpx::endl;
   
+#ifdef HPCG_DEBUG
   //varivate data
   for (local_int_t i=0; i<x.localLength; ++i){
     HPX_ASSERT(x_ref.values[i] == x.values[i]);
   }
-  hpx::cout << "SPMV test ok!\n";
+  hpx::cerr << "SPMV test ok!\n";
+#endif
   hpx::cout << "ref_time was " <<ref_time << " opt_time was " << opt_time <<hpx::endl;
+
 #else
 
   std::cout << "SPMV toks " <<ref_time << std::endl;
